bd63e232f003691a6740200425edacfa6e4867d4
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-08 21:07:42 -0500

    tipos de datos

--- num.php
+++ num.php
@@ -12,5 +12,23 @@
 
     echo "\u{10084}";
 
+    //datos compuestos
+
+    $arreglo = [ 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado' ];
+
+
+    $division = 10 / 3;
+
+    echo $division . '<br>';
+    echo  '   ';
+    echo (integer) $division . '<br>';
+    $entero =  (int) $division;  
+
+    var_dump( $division );
+    echo '<br>';
+    var_dump($entero);
+    echo '<br>';
+
+    echo gettype($entero);
 
 ?>

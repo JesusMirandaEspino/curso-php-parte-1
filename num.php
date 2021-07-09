0bdf2781f5dd07bd41d51a6357129240024b9cb8
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-08 22:04:52 -0500

    operadores

--- num.php
+++ num.php
@@ -29,6 +29,54 @@
     var_dump($entero);
     echo '<br>';
 
-    echo gettype($entero);
+    echo gettype($entero). '<br>'  ;
+
+
+    //Operadores
+
+    $a = 5;
+
+
+    echo $a % 2 . '<br>'  ;
+    echo $a ** 3 . '<br>'; 
+
+    $a += 5;
+    $a -= 5;
+    $a /= 5;
+
+    $c = 5;
+    $b = "5";
+    $d = 5;
+
+    echo $c == $b;
+
+    if($c == $b && $d == $a ){
+        echo 'Son iguales';
+    }else{
+        echo 'Son diferentes';
+    }
+
+    if ($c === $b || $d === $a) {
+        echo 'Son iguales.';
+    } else {
+        echo 'Son diferentes.';
+    }
+
+    if ($c != $b ) {
+        echo 'Son iguales';
+    } else {
+        echo 'Son diferentes';
+    }
+
+    $c++;
+    ++$d;
+
+    $mensaje = 'Hola ';
+    $mensaje .= 'Mundo';
+
+    echo $mensaje;
+
 
 ?>
+
+

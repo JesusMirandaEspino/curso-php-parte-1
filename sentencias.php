396252bcd8d1af752dfcac9ac457d5fd7aa1e29d
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-13 21:31:14 -0500

    ciclos

--- sentencias.php
+++ sentencias.php
@@ -9,6 +9,8 @@ $b = 10;
 $total = 123456;
 $tipocambio = 19.45;
 $grantotal = ($tipocambio > 0 ) ? $total * $tipocambio :  $total;
+$semana = [ 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo' ];
+
 
     if( $edad < 18 ){
         echo 'Eres menor de edad';
@@ -41,6 +43,46 @@ $grantotal = ($tipocambio > 0 ) ? $total * $tipocambio :  $total;
         default: break;
     }
 
+    $contador = 0;
+    $valor1 = 0;
+    $valor2 = 1;
+    $temp = 0;  
 
+echo  '   ' . $temp . ' ';
+
+    $valora = 0;
+    $valorb = 1;
+    $temp1 = 0;
+    $contadora = 0;
+
+echo  '   ' . $temp1 . ' ';
+
+    while( $contador < 25 ){
+
+        $temp = $valor1 + $valor2;
+        $valor1 = $valor2;
+        $valor2 = $temp;
+
+        echo $temp . ' ';
+
+        $contador++;
+    }
+
+
+
+    do{
+        $temp1 = $valora + $valorb;
+        $valora = $valorb;
+        $valorb = $temp1;
+
+        echo $temp1 . ' ';
+
+        $contadora++;
+    }while( $contadora < 30 );
+
+
+    for( $i = 0; $i < count( $semana ); $i++  ){
+        echo '  ' . $semana[$i] . ' ';
+    }
 
 ?>

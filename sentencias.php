9a9d61b964b4b69dbf4850def67d3153b5ffed07
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-13 20:57:56 -0500

    sentencia switch

--- sentencias.php
+++ sentencias.php
@@ -1,10 +1,14 @@
 <?php
 
+$entrada = 3;
 $edad = 16;
 $edad2 = 24;
 $casado = true;
 $a = 5;
 $b = 10;
+$total = 123456;
+$tipocambio = 19.45;
+$grantotal = ($tipocambio > 0 ) ? $total * $tipocambio :  $total;
 
     if( $edad < 18 ){
         echo 'Eres menor de edad';
@@ -23,6 +27,19 @@ $b = 10;
     else : echo 'Es menor' ;
     endif;
 
+    switch($entrada){
+
+    case 1: echo 'Valor de entrada 1';
+        break;
+    case 2:
+        echo 'Valor de entrada 2';
+        break;
+    case 3:
+        echo 'Valor de entrada 3';
+        break;
+
+        default: break;
+    }
 
 
 

1a090e3a30698b170d36243f1195aefdc11a7de3
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-20 21:00:47 -0500

    arreglos asociativos

--- list.php
+++ list.php
@@ -3,6 +3,8 @@
     $salto = '</br>';
 
     $lista = ['1', '2', '3', '4', '5'];
+    $numeros = [ 'uno' => '1',  'dos' => '2',  'tres' => '3',  'cuatro' => '4',  'cinco' => '5' ];
+    $numeros2 = ['seis' => '6',  'siete' => '7',  'ocho' => '8',  'nueve' => '9',  'cero' => '0'];
 
     list( $uno, $dos, $tres, $cuatro, $cinco  ) = $lista;
 
@@ -12,6 +14,18 @@
     echo $cuatro . $salto;
     echo $cinco . $salto;
 
+    print_r (array_values( $numeros ));
 
+    echo  $salto;
+
+    print_r(array_keys($numeros));
+
+    echo  $salto;
+
+    print_r(array_keys($numeros, '3'));
+
+    echo  $salto;
+
+    print_r(array_combine($numeros, $numeros2));
 
 ?>

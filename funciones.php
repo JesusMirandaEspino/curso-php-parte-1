3254878a92aba25d25e03d20a908d4218e8b55c7
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-20 21:47:17 -0500

    funciones parametros

--- funciones.php
+++ funciones.php
@@ -2,7 +2,7 @@
 
     $nombre = 'Jesus';
     $mensaje = 'Hola a todos';
-    
+
 
 
     function saludar($nombre){
@@ -10,12 +10,20 @@
         echo '<h2> ' .  $mensaje . '  soy  ' .  $nombre  .' </h2>' . '</br>';
     }
 
+    function saludos( &$nom)
+    {
+        $nom .= ', saluda a todos';
+        echo $nom . '</br>';
+    }
 
-    function areaRec( $num1, $num2 ){
+
+    function areaRec( $num1, $num2 = 5 ){
         $num3 = $num1 * $num2;
         return $num3;
     }
 
+$nom = 'Mutuelo';
+    saludos( $nom );
 
     saludar('Mutuelo');
 

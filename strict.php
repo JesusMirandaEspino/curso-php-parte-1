dc93152073208b2691e9b4752142f8c86191d83c
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-07-22 21:18:13 -0500

    yield

--- strict.php
+++ strict.php
@@ -36,7 +36,38 @@ try{
     echo $e->getMessage();
 }
 
+
+$input = <<< 'EOF'
+1;Mutuelo;Mago
+2;Ramo;Guerrero
+EOF;
+
+function personas($input){
+    foreach( explode('\n', $input ) as $val ){
+        $r = explode(';', $val );
+        $id = array_shift($r);
+        yield $id=>$r;
+    }
+}
+
+foreach ( personas($input) as $va => $persona ) {
+    echo $va . '</br>';
+    echo $persona[0] . '</br>';
+    echo $persona[1] . '</br>';
+}
+
 recorrer( 1,2,4,5,6,4,6,7,4 );
+
+$gen = ( function (){
+    yield 1;
+    yield 2;
+    return 3;
+} );
+
+
+foreach( $gen as $v){
+    echo  '<p>' .  $v .  '</p>';
+} 
 
 
 ?>
